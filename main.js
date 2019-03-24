1b43c58c8a0b6687494b4da8755a9010bf2cfba5
Josiah Sayers <josiah.sayers15@gmail.com>
2019-03-23 21:12:07 -0400

    Added reminder text to turn off silent mode and turn up volume

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault();
   let textBox = document.getElementById('textInput');
-  responsiveVoice.speak(textBox.value, 'US English Female');
+  responsiveVoice.speak(textBox.value);
 }, true);
